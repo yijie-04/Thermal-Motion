f1cdf47a3417d3030cab7af246042dd9f67b0e20
Yijie Wang <147119743+yijie-04@users.noreply.github.com>
2024-02-23 12:21:45 -0500

    Update Mean Squared VS Distance.py

--- src/Mean Squared VS Distance.py
+++ src/Mean Squared VS Distance.py
@@ -4,55 +4,95 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import loadtxt
 import math
+import matplotlib.colors as mcolors
 
-def dist(x1, x2, y1, y2):
-    return math.sqrt((x2-x1)**2+(y2-y1)**2)
+file_names = ["/Users/yijiewang/Documents/WINTER 2024/PHY294/Thermal Motion/bead1.txt"]
+t = []
+d = []
+data = loadtxt(file_names[0], usecols=(0, 1), skiprows=2, unpack=True)
+x_data = data[0, :]
+y_data = data[1, :]
+def distance_travelled(x1, y1, x2, y2):
+   conv = 0.1155  #10^-6 m
+   dx = x1-x2
+   dy = y1-y2
+   dx = conv*dx
+   dy = conv*dy
+   return (dx**2 + dy**2)**0.5
 
-def rayleigh_dist(r, sigma):
-    return (r / (sigma**2)) * np.exp(-(r**2) / (2 * sigma**2))
+def dist(x1, y1, x2, y2):
+   conv = 0.1155  #10^-6 m
+   dx = x1-x2
+   dy = y1-y2
+   dx = conv*dx
+   dy = conv*dy
+   return (dx**2 + dy**2)**0.5
+for i in range(len(x_data)-1):
+   if i == 0:
+      t.append(0)
+      d.append(dist(x_data[i], y_data[i],x_data[i+1], y_data[i+1]))
+      dist(x_data[i], y_data[i],x_data[i+1], y_data[i+1])
+   else:
+      d.append(dist(x_data[i], y_data[i],x_data[i+1], y_data[i+1]) + d[-1])
+      dist(x_data[i], y_data[i],x_data[i+1], y_data[i+1])
+      t.append(t[-1] + 0.5)
 
-def main():
-    step_size = []
+xdata = np.array(t)
+xerror = 0.03
+yerror = 0.1
+ydata = np.array(d)
 
-    for i in range(1, 30):
-        if i == 5:
-            continue
 
-        filename=f"/Users/yijiewang/Documents/WINTER 2024/PHY294/Thermal Motion/bead{i}.txt"
+def my_func(t, m):
+    return t*m 
 
-        data=loadtxt(filename, usecols=(0,1), skiprows=2, unpack=True)
+popt, pcov = curve_fit(my_func, xdata, ydata)
+a=popt[0]
+u_a = pcov[0, 0]
+print(a, u_a)
 
-        xdata = data[0]
-        ydata = data[1]
-        for j in range(len(xdata)-1):
-            step_size.append(dist(xdata[j], xdata[j+1], ydata[j], ydata[j+1]))
-    print(len(step_size))
-    step_size = np.array(step_size)
-    # hist, bins = np.histogram(step_size, bins=50, density=True)
-    # bin_centers = 0.5 * (bins[1:] + bins[:-1])
-    bin_width = 0.1  # Set the desired bin width
-    min_edge = min(step_size)  # Find the minimum value in the data
-    max_edge = max(step_size)  # Find the maximum value in the data
-    bins = np.arange(min_edge, max_edge + bin_width, bin_width)
-    bin_centers = 0.5 * (bins[1:] + bins[:-1])
-    hist, bins = np.histogram(step_size, bins=bins, density=True)
-    # Create a range of bin edges from min to max with the specified width
+plt.rcParams.update({'font.size': 14})
+plt.rcParams['figure.figsize'] = 10, 9
 
-    plt.hist(step_size, bins=bins, density=True, alpha=0.6, color='g')
+start = min(xdata)
+stop = max(xdata)
+xs = np.arange(start,stop,(stop-start)/1000)
+curve = my_func(xs, *popt)
+fig, (ax1,ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1.6, 1]})
 
-    init_guess = np.sqrt(np.mean(step_size ** 2) / 2)
+ax1.errorbar(xdata, ydata, yerr=yerror, xerr=xerror, fmt=".", label="data", color="blue")
+ax1.plot(xs, curve, label="best fit", color="black")
+ax1.legend(loc='upper right')
 
-    params, covariance = curve_fit(rayleigh_dist, bin_centers, hist, p0=[init_guess])
-    sigma_fit, sigma_fit_error = params[0], np.sqrt(covariance[0, 0])
-    print(f'Sigma value from Rayleigh distribution fit: {sigma_fit} micrometers')
-    print(f'Uncertainty for sigma: {sigma_fit_error} micrometers')
+ax1.set_xlabel("Time [s]")
+ax1.set_ylabel("Mean Squared Distance [μm]")
+ax1.set_title("Step Size Versus Time plot for a Sample Bead Motion")
 
-    # Plot the fitted Rayleigh distribution curve
-    r = np.linspace(0, data.max(), 100)
-    plt.plot(r, rayleigh_dist(r, sigma_fit), 'r-', label='Fitted Rayleigh distribution')
+residual = ydata - my_func(xdata, popt[0])
+ax2.errorbar(xdata, residual, yerr=yerror, xerr=xerror, fmt=".", color="blue")
+# Plot the residuals with error bars.
 
-    plt.axis([0, 20, 0, 0.2])  # Set x-axis from 0 to 6 and y-axis from 0 to 30
+y_perdict = my_func(xdata, a)
+corr_matrix = np.corrcoef(ydata, y_perdict)
+corr = corr_matrix[0,1]
+R_sq = corr**2
+print("R_sq1", R_sq)
 
-    plt.show()
+deltay = 0.1
+chi = 0
+for i in range(len(y_perdict)):
+   deltay = (deltay**2+0.1**2)**0.5
+   chi = chi + ((ydata[i] - y_perdict[i])**2)/(deltay)
 
-main()
+chi = chi/((deltay**2) * (len(y_perdict) - 1))
+print("Chi1",chi)
+
+
+ax2.axhline(y=0, color="black")
+
+ax2.set_xlabel("Time [s]")
+ax2.set_ylabel("Mean Squared Distance [μm]")
+ax2.set_title("Residuals")
+
+plt.tight_layout()
+plt.show()
